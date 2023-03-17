3b3193cd9d96ced522a1d95493f81af68667522f
cecilia-89 <catabong89@gmail.com>
2023-03-18 00:01:13 +0100

    implemented dropdown for user profile

--- chatt-frontend/src/components/Navbar/navbar.jsx
+++ chatt-frontend/src/components/Navbar/navbar.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
+import React from 'react';
 import './navbar.scss'
 
 function Navbar() {
+
+    const [state, setState] = useState(false)
+    const visible = () => setState(!state)
+
     return (
         <nav>
             <div> Chatt </div>
+            <div>
+                <div onClick={visible}>
+                    <ion-icon name="ellipsis-vertical"></ion-icon>
+                </div>
+                <div className={state ? "dropdown": "hidden"}>
+                <ul>
+                    <li>Change Profile</li>
+                    <li>Log Out</li>
+                </ul>
+            </div>
+            </div>
         </nav>
      );
 }
--- chatt-frontend/src/components/Sidebar/sidebar.jsx
+++ chatt-frontend/src/components/Sidebar/sidebar.jsx
@@ -13,7 +13,6 @@ const Sidebar = () => {
                     <ion-icon name="search-outline"></ion-icon>
                 </div>
 
-
                 <div className='chats-wrapper'>
                     <div>
                         <div>
@@ -101,6 +100,25 @@ const Sidebar = () => {
                             </div>
                     </div>
                 </div>
+
+                <div className="search-wrapper hidden">
+                <div>
+                        <div>
+                            <img src={IMG1} alt="" />
+                            <div className='notifications'>
+                                <div>99</div>
+                            </div>
+                        </div>
+                        <div className='details'>
+                            <span>Cecilia Atabong</span>
+                            <p>This is the last time it is yes it is</p>
+                        </div>
+                        <div className="timestamp">
+                            <span>3:45</span>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </section>
         );

b97e861408876b0ed3eb9ccd0b943af05e407337
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-03-27 20:23:10 +0100

    fixing mailing function

--- chatt-backend/utils/mailer.js
+++ chatt-backend/utils/mailer.js
@@ -2,13 +2,13 @@ import nodemailer from 'nodemailer';
 import sendGrid from '@sendgrid/mail';
 
 const mailer = async (email, subject, body) => {
-  // const transporter = nodemailer.createTransport({
-  //   service: 'gmail',
-  //   auth: {
-  //     user: 'web.chattmessaging@gmail.com',
-  //     pass: 'iplptuaoprqkyjkr'
-  //   }
-  // });
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'web.chattmessaging@gmail.com',
+      pass: 'iplptuaoprqkyjkr'
+    }
+  });
 
   sendGrid.setApiKey(process.env.SENDGRID_API_KEY)
   const message = {
@@ -17,52 +17,53 @@ const mailer = async (email, subject, body) => {
     subject: subject,
     html: body,
   }
-  await sendGrid.send(message, (error, result) => {
-    if (error) {
-      console.log(error);
-     } else {
-      console.error('Email sent!', result);
-     }
-  });
 
-  // const mailOptions = {
-  //   from: 'Chatt Instant Messaging',
-  //   to: email,
-  //   subject: subject,
-  //   text: body
-  // };
+  const mailOptions = {
+    from: 'Chatt Instant Messaging',
+    to: email,
+    subject: subject,
+    html: body
+  };
 
-  // new Promise((resolve, reject) => {
-  //   transporter.verify((error, success) => {
-  //     if (error) {
-  //       reject(error);
-  //     } else {
-  //       resolve(success);
-  //     }
-  //   });
-  // })
-  //   .then(() => {
-  //     console.log('Nodemailer Conection Established');
-  //   })
-  //   .catch((err) => {
-  //     console.llog('Error connecting to Nodemailer:', err);
-  //   });
+  new Promise((resolve, reject) => {
+    transporter.verify((error, success) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(success);
+      }
+    });
+  })
+    .then(() => {
+      console.log('Nodemailer Conection Established');
+    })
+    .catch((err) => {
+      console.llog('Error connecting to Nodemailer:', err);
+    });
 
-  // new Promise((resolve, reject) => {
-  //     transporter.sendMail(mailOptions, (error, info) => {
-  //       if (error) {
-  //         reject(error);
-  //       } else {
-  //         resolve(info.response);
-  //       }
-  //     });
-  // })
-  //   .then((response) => {
-  //     console.log('Email sent:' + response);
-  //   })
-  //   .catch((err) => {
-  //     console.log(error);
-  //   });
+  new Promise((resolve, reject) => {
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(info.response);
+        }
+      });
+  })
+    .then((response) => {
+      console.log('Email sent:' + response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+    await sendGrid.send(message, (error, result) => {
+      if (error) {
+        console.log(error);
+       } else {
+        console.error('Email sent!', result);
+       }
+    });
 }
 
 export default mailer;

5d1c554a822c31ac7ac3c780bd0dd8217cf5739d
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-03-27 18:06:16 +0100

    socket.io in progress

---
+++ chatt-backend/utils/socketio.js
@@ -0,0 +1,19 @@
+import socketio from 'socket.io';
+
+class SocketIO {
+
+    socketConnection (server) {
+        this.io = socketio(server, {
+            pingTimeout: 60000,
+            cors: {
+                origin: '0.0.0.0/0'
+            }
+        });
+        this.io.on('connection', (socket) => {
+            console.log('Connected to socket');
+        })
+    }
+}
+
+const socketIO = new SocketIO();
+export default socketIO;
--- chatt-frontend/src/axios.js
+++ chatt-frontend/src/axios.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL
+    // baseURL: import.meta.env.VITE_BASE_URL
+    baseURL: 'https://chatt.cyclic.app/api/v1'
 })
 
 export default axios;

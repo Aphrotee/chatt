11a1803e0dc8dfa657f7dbb5152a41dce5bf0dc9
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-03-28 16:23:37 +0100

    real-time messaging feature with socket.io in progress

--- chatt-backend/utils/socketio.cjs
+++ chatt-backend/utils/socketio.cjs
@@ -1,16 +1,55 @@
+const mongoose = require('mongoose');
+
 class SocketIO {
 
-    socketConnection (server) {
-        this.io = require('socket.io')(server, {
-            pingTimeout: 60000,
-            cors: {
-                origin: '0.0.0.0/0'
-            }
-        });
-        this.io.on('connection', (socket) => {
-            console.log('Connected to socket');
-        })
-    }
+  socketConnection (server) {
+    this.io = require('socket.io')(server, {
+      pingTimeout: 60000,
+      cors: {
+        origin: 'http://127.0.0.1:5173'
+      }
+    });
+    this.io.on('connection', (socket) => {
+      console.log('Connected to socket');
+
+      socket.on('user connect', (userId) => {
+        socket.join(userId);
+          socket.in(userId).emit('user connected', userId);
+      });
+
+      socket.on('open container', (containerId) => {
+        socket.join(containerId);
+        console.log('user opened container', containerId);
+      });
+
+      const db = mongoose.connection;
+      const messagecontainers = db.collection('messagecontainers');
+      const messageChangeStream = messages.watch();
+      const msgContChangeStream = messagecontainers.watch();
+      messageChangeStream.on('change', (change) => {
+        // console.log('Change occured:', change);
+        if (change.operationType === 'insert') {
+          const messageDetails = change.fullDocument;
+          const container = messageDetails.containerId;
+          socket.in(container).emit('new message', messageDetails);
+        }
+      });
+      msgContChangeStream.on('change', async (change) => {
+        console.log('Change occured:', change);
+        if (change.operationType === 'update') {
+          // const containerDetails = change.updateDescription.updatedFields;
+          const id = change.documentKey._id;
+          const { default: MessageContainerController } = await import('../models/MessageContainers.js');
+          MessageContainerController.findById(mongoose.Types.ObjectId(id))
+            .then((container) => {
+              container.members.forEach((member) => {
+                socket.in(member).emit('container updated', container);
+              });
+            })
+        }
+      })
+    });
+  }
 }
 
 const socketIO = new SocketIO();

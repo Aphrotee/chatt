68110aa6a8c9028366b07110ef0c83104a1582c8
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2024-08-03 17:28:37 +0100

    Update: updated base url in axios.js

--- chatt-frontend/src/axios.js
+++ chatt-frontend/src/axios.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 const axios = Axios.create({
     // baseURL: import.meta.env.VITE_BASE_URL
     // baseURL: 'http://172.29.220.240:9000/api/v1/'
-    baseURL: 'https://chatt.cyclic.app/api/v1'
+    baseURL: 'https://chatt-xjs2.onrender.com/api/v1'
 });
 
 export default axios;
